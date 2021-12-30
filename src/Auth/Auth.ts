6e8309c0151cf604e75ed7f13acf27bcdf4709d9
Ian <iwhite99@protonmail.com>
2021-12-30 14:06:00 -0600

    Update Auth.ts

--- src/Auth/Auth.ts
+++ src/Auth/Auth.ts
@@ -1,4 +1,4 @@
-import axios, {AxiosError, AxiosResponse} from 'axios';
+import axios, {AxiosError} from 'axios';
 import rs from 'jsrsasign';
 import ZoomInfoException from '../helpers/Exception/ZoomInfoException';
 
